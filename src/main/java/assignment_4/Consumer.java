b119debbe89f3b5011dcd4d285a00e1835c54ddd
GAnjali <anjali.gaddam98@gmail.com>
2018-11-09 00:12:20 +0530

    Adds closing of connections after consuming

--- src/main/java/assignment_4/Consumer.java
+++ src/main/java/assignment_4/Consumer.java
@@ -1,5 +1,8 @@
 package assignment_4;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 import static java.lang.Thread.sleep;
 
 public class Consumer implements Runnable {
@@ -11,11 +14,17 @@ public class Consumer implements Runnable {
 
     @Override
     public void run() {
+        Connection connection = null;
         while (true) {
-            buffer.consume();
+            connection = buffer.consume();
             try {
                 sleep(1000);
             } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            try {
+                connection.close();
+            } catch (SQLException e) {
                 e.printStackTrace();
             }
         }
